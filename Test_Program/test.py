e483870a4e318a54c37346f7982be6d8943a5d4d
Amrita <amrit.para@gmail.com>
2017-09-27 14:35:52 +0200

    update

---
+++ Test_Program/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+import ev3dev.ev3 as ev3
+
+ev3.Sound.speak('Welcome Group 5! If you can hear me that is a good thing').wait()
+time.sleep(3)
+ev3.Sound.speak('How are you?').wait()
