255c2cb2cd337f080650362c39f98c8eed8e0730
jiamingGithub <32131944+jiamingGithub@users.noreply.github.com>
2017-11-22 15:32:45 +0100

    Update MotorPID.py

--- Test_Program/MotorPID.py
+++ Test_Program/MotorPID.py
@@ -45,7 +45,7 @@ middle = (White - Black)/2 + Black
 lasterror = 0
 integral = 0
 
-speed = 450
+speed = -450
 # All the configurations are done so far
 try:
 	# Make a infinite loop
@@ -76,4 +76,6 @@ try:
 #		time.sleep(1)
 
 except KeyboardInterrupt:
-	m.stop()
+	m1.stop()
+	m2.stop()
+	m3.stop()
